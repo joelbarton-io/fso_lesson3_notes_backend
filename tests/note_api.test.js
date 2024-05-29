3d65db01e2a208932347303ada4060fd24596347
Joel Barton <joelbarton406@gmail.com>
2024-05-28 18:07:36 -0600

    exercise 4.12

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,6 +8,119 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 
+describe('when there is initially some notes saved', () => {
+  beforeEach(async () => {
+    await Note.deleteMany({})
+    await Note.insertMany(helper.initialNotes)
+  })
+
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/notes')
+
+    assert.strictEqual(response.body.length, helper.initialNotes.length)
+  })
+
+  test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/notes')
+
+    const contents = response.body.map((r) => r.content)
+    assert(contents.includes('Browser can execute only JavaScript'))
+  })
+
+  describe('viewing a specific note', () => {
+    test('succeeds with a valid id', async () => {
+      const notesAtStart = await helper.notesInDb()
+
+      const noteToView = notesAtStart[0]
+
+      const resultNote = await api
+        .get(`/api/notes/${noteToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.deepStrictEqual(resultNote.body, noteToView)
+    })
+
+    test('fails with statuscode 404 if note does not exist', async () => {
+      const validNonexistingId = await helper.nonExistingId()
+
+      await api.get(`/api/notes/${validNonexistingId}`).expect(404)
+    })
+
+    test('fails with statuscode 400 id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445'
+
+      await api.get(`/api/notes/${invalidId}`).expect(400)
+    })
+  })
+
+  describe('addition of a new note', () => {
+    test('succeeds with valid data', async () => {
+      const newNote = {
+        content: 'async/await simplifies making async calls',
+        important: true,
+      }
+
+      await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const notesAtEnd = await helper.notesInDb()
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
+
+      const contents = notesAtEnd.map((n) => n.content)
+      assert(contents.includes('async/await simplifies making async calls'))
+    })
+
+    test('fails with status code 400 if data invalid', async () => {
+      const newNote = {
+        important: true,
+      }
+
+      await api.post('/api/notes').send(newNote).expect(400)
+
+      const notesAtEnd = await helper.notesInDb()
+
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+    })
+  })
+
+  describe('deletion of a note', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const notesAtStart = await helper.notesInDb()
+      const noteToDelete = notesAtStart[0]
+
+      await api.delete(`/api/notes/${noteToDelete.id}`).expect(204)
+
+      const notesAtEnd = await helper.notesInDb()
+
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
+
+      const contents = notesAtEnd.map((r) => r.content)
+      assert(!contents.includes(noteToDelete.content))
+    })
+
+    test('fails with status code 400 if id is invalid', async () => {
+      const invalidID = '5a3d5da59070081a82a3445'
+      await api.delete(`/api/notes/${invalidID}`).expect(400)
+    })
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
+/*
 beforeEach(async () => {
   await Note.deleteMany({})
   const noteObjects = helper.initialNotes.map((note) => new Note(note))
@@ -36,34 +149,34 @@ describe('General', () => {
 })
 
 describe('Routes ', () => {
-  test('POST (a valid note)...', async () => {
-    const dummyNote = {
+  test('POST...a valid note', async () => {
+    const validNote = {
       content: 'async/await simplifies making async calls',
       important: true,
     }
 
     await api
       .post('/api/notes')
-      .send(dummyNote)
+      .send(validNote)
       .expect(201)
       .expect('Content-Type', /application\/json/)
     const notesAtEnd = await helper.notesInDb()
     const contents = notesAtEnd.map((r) => r.content)
 
     assert.equal(notesAtEnd.length, helper.initialNotes.length + 1)
-    assert(contents.includes(dummyNote.content))
+    assert(contents.includes(validNote.content))
   })
 
-  test('POST (a note without content)...', async () => {
-    const dummyNote = { important: false }
+  test('POST...an invalid note without content', async () => {
+    const invalidNote = { important: false }
 
-    api.post('/api/notes').send(dummyNote).expect(400)
+    api.post('/api/notes').send(invalidNote).expect(400)
 
     const notesAtEnd = await helper.notesInDb()
     assert.ok(notesAtEnd.length, helper.initialNotes.length)
   })
 
-  test('GET (a valid note)', async () => {
+  test('GET...a valid note', async () => {
     const notes = await helper.notesInDb()
     const note = notes.at(0)
     const res = await api
@@ -75,7 +188,17 @@ describe('Routes ', () => {
     assert.deepStrictEqual(res.body, note)
   })
 
-  test('Delete (a valid note)...', async () => {
+  test('GET...fails with statuscode 404 if note has valid (bson) but non-existing ID', async () => {
+    const id = await helper.nonExistingId()
+    await api.get(`/api/notes/${id}`).expect(404)
+  })
+
+  test('GET...fails with status code 404 if note has an invalid, non-existing ID', async () => {
+    const id = '123'
+    await api.get(`/api/notes/${id}`).expect(400)
+  })
+
+  test('DELETE...a valid note', async () => {
     const notesStart = await helper.notesInDb()
     const note = notesStart.at(0)
     const ID = note.id
@@ -87,15 +210,17 @@ describe('Routes ', () => {
     assert(!contents.includes(note.content))
     assert.ok(notesEnd.length, helper.initialNotes.length - 1)
   })
-  //   test('Delete (an invalid note)', async () => {
-  //     const fakeID = await helper.nonExistingId()
-  //     const notesStart = await helper.notesInDb()
-  //     await api.delete(`/api/notes/${fakeID}`).expect(404)
-  //     const notesEnd = await helper.notesInDb()
-  //     assert.ok(notesStart.length, notesEnd.length)
-  //   })
+
+  test('DELETE...an invalid note', async () => {
+    const fakeID = await helper.nonExistingId()
+    const notesStart = await helper.notesInDb()
+    await api.delete(`/api/notes/${fakeID}`).expect(404)
+    const notesEnd = await helper.notesInDb()
+    assert.ok(notesStart.length, notesEnd.length)
+  })
 })
 
 after(async () => {
   await mongoose.connection.close()
 })
+*/
